fba7768ae4ba7b1e7525bb18572260930dcdd833
lilachaimo <lilachaimo@users.noreply.github.com>
2018-12-26 20:32:24 +0200

    first upload

---
+++ files-download/src/main/java/com/yonashlilach/filesdownloadproject/FilesDownloadProjectApplication.java
@@ -0,0 +1,14 @@
+package com.yonashlilach.filesdownloadproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilesDownloadProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilesDownloadProjectApplication.class, args);
+	}
+
+}
+
